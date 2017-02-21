350a0f5bf937f5d261907e265cbebe1137028a68
hgu <hgu@coupang.com>
2017-02-21 17:50:27 +0800

    to mediam questions

---
+++ src/com/harper/functional/swap_nodes.scala
@@ -0,0 +1,17 @@
+package com.harper.functional
+
+/**
+  * Created by harperkoo on 2/21/17.
+  * https://www.hackerrank.com/challenges/swap-nodes
+  */
+object swap_nodes {
+  def main(args: Array[String]): Unit = {
+    val N = scala.io.StdIn.readInt()
+    //read n nodes
+    //TODO
+
+    val T = scala.io.StdIn.readInt()
+    //read t swaps and print the inorder
+    //TODO
+  }
+}
