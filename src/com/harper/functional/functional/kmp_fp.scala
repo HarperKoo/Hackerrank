45c1abb9da48b4a8aea51139024d16857b8de031
hgu <hgu@coupang.com>
2017-08-18 18:30:30 +0800

    add kmp_fp

---
+++ src/com/harper/functional/functional/kmp_fp.scala
@@ -0,0 +1,60 @@
+package com.harper.functional.functional
+
+/**
+  * Created by harperkoo on 8/17/17.
+  * https://www.hackerrank.com/challenges/kmp-fp
+  * https://hr-testcases-us-east-1.s3.amazonaws.com/2175/input05.txt?AWSAccessKeyId=AKIAJ4WZFDFQTZRGO3QA&Expires=1503058467&Signature=LirdGpwcr7ailpBaWpQecv2uWL0%3D&response-content-type=text%2Fplain
+  * https://hr-testcases-us-east-1.s3.amazonaws.com/2175/output05.txt?AWSAccessKeyId=AKIAJ4WZFDFQTZRGO3QA&Expires=1503058469&Signature=5R78khVkH9DJnrXKzSMeCk%2BR%2FE0%3D&response-content-type=text%2Fplain
+  */
+object kmp_fp {
+
+  def kmp_search(text:String, pat:String):Boolean = {
+    var i = 0
+    while( i < text.length){
+      var sub_str = ""
+      var common_length = 0
+      var j = 0
+      while(text(i) == pat(j)){
+        if(j < pat.length-1){
+          sub_str += text(i)
+          common_length += 1
+          i += 1//TODO
+          j += 1
+        }else return true
+      }
+
+      val move_index = if(sub_str.length > 1) common_length - sub_match(sub_str) else 0
+      println(i,common_length,sub_str,move_index)
+      i += (if(move_index == 0) 1 else move_index)//TODO
+    }
+    false
+  }
+
+
+  def sub_match(sub_str:String):Int = {
+    val pre = prefix(sub_str)
+    val suf = suffix(sub_str)
+    val compare_ps = pre.zip(suf).filter(x=>x._1 == x._2)
+    if( compare_ps.isEmpty ) 0 else compare_ps.head._1.length
+  }
+
+  def prefix(sub_str:String):List[String] = {
+    val pre_sub_str = sub_str.substring(0,sub_str.length-1)
+    if(pre_sub_str.length == 1) List(pre_sub_str) else pre_sub_str::prefix(pre_sub_str)
+  }
+
+  def suffix(sub_str:String):List[String] = {
+    val suf_sub_str = sub_str.substring(1,sub_str.length)
+    if(suf_sub_str.length == 1) List(suf_sub_str) else suf_sub_str::suffix(suf_sub_str)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val T = scala.io.StdIn.readInt()
+    (1 to T).foreach(_ => {
+      val text = scala.io.StdIn.readLine()
+      val pat = scala.io.StdIn.readLine()
+      println(if(kmp_search(text,pat)) "YES" else "NO")
+    }
+    )
+  }
+}
---
+++ src/com/harper/functional/functional/swap_nodes.scala
@@ -0,0 +1,85 @@
+package com.harper.functional.functional
+
+/**
+  * Created by harperkoo on 7/27/17.
+  * https://www.hackerrank.com/challenges/swap-nodes
+  */
+
+import java.util.Scanner
+
+object Solution extends App {
+
+  object Node {
+    def emptyNode(value: Int, depth: Int): Node = {
+      new Node(value, None, None, depth)
+    }
+
+    def buildNode(v: Int, d: Int): Option[Node] = {
+      if (v == -1) {
+        None
+      } else {
+        Some(emptyNode(v, d))
+      }
+    }
+  }
+
+  class Node(value: Int, left: Option[Node], right: Option[Node], depth: Int) {
+    var swapped = true
+    def swap(d: Int) {
+      if (depth % d == 0) {
+        swapped = !swapped
+      }
+      left map { l =>
+        l.swap(d)
+      }
+      right map { r =>
+        r.swap(d)
+      }
+    }
+
+    def apply(v: Int, p: (Int, Int)): Node = {
+      if (v == value) {
+        new Node(value, Node.buildNode(p._1, depth+1), Node.buildNode(p._2, depth+1), depth)
+      } else {
+        val newLeft = left map { l =>
+          l.apply(v, p)
+        }
+        val newRight = right map { r =>
+          r.apply(v, p)
+        }
+        new Node(value, newLeft, newRight, depth)
+      }
+    }
+
+    def traverse(): String = {
+      val leftT = left match {
+        case Some(n) => n.traverse()
+        case None => ""
+      }
+      val rightT = right match {
+        case Some(n) => n.traverse()
+        case None => ""
+      }
+      if (swapped) {
+        (leftT + " " + value + " " + rightT).trim
+      } else {
+        (rightT + " " + value + " " + leftT).trim
+      }
+    }
+  }
+
+  val in = new Scanner(System.in)
+  val N = in.nextInt()
+  val rootNode = (1 to N).foldLeft(Node.emptyNode(1, 1))((node, idx) => {
+    val a,b = in.nextInt()
+    val pair = (a,b)
+    node.apply(idx, pair)
+  })
+  val K = in.nextInt()
+  (0 until K).foldLeft(rootNode)((node, _) => {
+    val idx = in.nextInt()
+    node.swap(idx)
+    println(node.traverse())
+    node
+  })
+}
